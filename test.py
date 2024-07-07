658927a808bab026b32576458be0f1c2a2aca478
nbaron17 <nbaron017@gmail.com>
2024-07-07 20:04:42 +0100

    added seperate test.py file

--- test.py
+++ test.py
@@ -17,21 +17,26 @@ os.makedirs(model_dir, exist_ok=True)
 os.makedirs(log_dir, exist_ok=True)
 
 
-def train(env):
-    model = PPO('MlpPolicy', env, verbose=1, device='cuda', tensorboard_log=log_dir)
-    # model = PPO.load('models/PPO_300000.zip', env=env, device='cuda', tensorboard_log=log_dir)
-    TIMESTEPS = 50000
-    iters = 0
+def test(env, path_to_model):
+    model = PPO.load(path_to_model, env=env)
+
+    # env.reset()
+    obs = env.reset()[0]
+    done = False
+    extra_steps = 5000
     while True:
-        iters += 1
-        model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
-        model.save(f"{model_dir}/PPO_{TIMESTEPS*iters}")
+        action, _ = model.predict(obs)
+        print(action)
+        obs, _, done, _, _ = env.step(action)
+        if done:
+            extra_steps -= 1
+            if extra_steps < 0:
+                break
 
 
 def main():
-    env = DummyVecEnv([lambda: Monitor(gym.make("GymV21Environment-v0", env_id="MinitaurBulletEnv-v0"))])
-    # check_env(gym.make('MinitaurBulletEnv-v0'))
-    train(env)
+    env = gym.make("GymV21Environment-v0", env_id="MinitaurBulletEnv-v0")
+    test(env, path_to_model="models/PPO_300000.zip")
 
 
 if __name__ == '__main__':
